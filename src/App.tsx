0e44932f96ef54e07ca529f13e24dc6e27fbd223
lilianguima <lilianm.guima@gmail.com>
2022-11-24 11:57:45 -0300

    react 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './componentes/navbar/Navbar';
 import Home from './componentes/home/Home';
 
 function App() {
   return (
    <>
-   <Home />
+   <Home/>
    </>
   );
 }
