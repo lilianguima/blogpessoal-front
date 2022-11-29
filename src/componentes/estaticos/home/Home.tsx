473322bc75c8897722283bb4e4f3963e3f25a48d
lilianguima <lilianm.guima@gmail.com>
2022-11-29 14:13:38 -0300

    routes

--- src/componentes/estaticos/home/Home.tsx
+++ src/componentes/estaticos/home/Home.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import './Home.css';
 
 function Home () {
+  const [num,setNum] = useState (0);
+
   return (
-    <h1>
-      Pagina Home</h1>
-  );
+    <>
+    <h1> Você clicou {num} vezes </h1>
+    <button onClick={() => setNum(num + 1)}> Clique aqui </button>
+    </>
+    );
 }
 export default Home
