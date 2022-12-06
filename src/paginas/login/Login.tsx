e88715f774247c337e228985cada6c4fc6b475fe
lilianguima <lilianm.guima@gmail.com>
2022-12-06 14:51:28 -0300

    att.01

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, ChangeEvent } from 'react';
-import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
-import { Link, useNavigate } from 'react-router-dom';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import {Box} from '@mui/material';
+import { Link, useNavigate  } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
 import { login } from '../../services/Service';
 import UserLogin from '../../models/UserLogin';
